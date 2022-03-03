55240a5781f39b8512a5f6dd744cbe0c4a490d38
Greg Thompson <greg@thegregthompson.com>
2022-03-03 13:54:03 -0500

    removed action buttons and fixed icon link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,26 +7,12 @@ export default function Header() {
       <nav className='mx-auto px-4 sm:px-6 lg:px-8' aria-label='Top'>
         <div className='flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none'>
           <div className='flex items-center'>
-            <span className='text-4xl'>&#128176;</span>
+            <Link href="/coins"><span className='text-4xl'>&#128176;</span></Link>
             <h1 className='ml-3 hidden text-xl text-white lg:inline-block'>
               <Link href="/coins">Greg&apos;s Cool Coins!</Link>
             </h1>
           </div>
           <div className='flex items-center'>{/* nav stuff */}</div>
-          <div className='ml-10 space-x-4'>
-            <Link
-              href='#'
-              className='inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75'
-            >
-              Action
-            </Link>
-            <Link
-              href='#'
-              className='inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50'
-            >
-              Action
-            </Link>
-          </div>
         </div>
       </nav>
     </header>
